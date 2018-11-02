be6560d9188438e540b0b759d1c45492d3b68cf9
Iaroslav Fadin <iaroslav.fadin@zalando.de>
2018-11-02 17:28:55 +0100

    Switched to luminol

--- detect.py
+++ detect.py
@@ -1,1 +1,15 @@
-from pyramid.arima import auto_arima
+from datetime import datetime
+from typing import List
+
+import pandas as pd
+from luminol import anomaly_detector
+from luminol.modules.anomaly import Anomaly
+
+sold_items = 'data/sold-items.csv'
+pd.read_csv(sold_items)
+
+detector = anomaly_detector.AnomalyDetector(time_series=sold_items)
+anomalies: List[Anomaly] = detector.get_anomalies()
+for a in anomalies:
+    dd = datetime.utcfromtimestamp(a.exact_timestamp).strftime('%Y-%m-%d %H:%M:%S')
+    print(f'{dd}: {a.anomaly_score}')
